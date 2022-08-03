aaa79cf66fe8e384d38242969ad81ef8d98e921c
Chembra <Jayaraj.Chembra@lamresearch.com>
2022-08-03 12:26:03 +0530

    my first version

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+
+    'lam/fin/ar/controller/BaseController'
+
+], function(BaseController) {
+
+    'use strict';
+
+    return BaseController.extend("lam.fin.ar.controller.App",{
+
+        onInit: function(){
+
+           
+
+        }
+
+    });
+
+});
+
